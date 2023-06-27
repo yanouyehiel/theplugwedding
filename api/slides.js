f2bf8347eacc300f32cf12dfc64fc2c5ea73d7d5
Yanou Yehiel <85465004+yanouyehiel@users.noreply.github.com>
2023-06-27 17:06:54 +0100

    Add files via upload
    Ajout du fichier slides.js dans ./api

---
+++ api/slides.js
@@ -0,0 +1,160 @@
+const Slides = [
+    {
+        id: 1,
+        path: '/images/slider/Slider-1.jpg'
+    },
+    {
+        id: 2,
+        path: '/images/slider/Slider-2.jpg'
+    },
+    {
+        id: 3,
+        path: '/images/slider/Slider-3.jpg'
+    },
+    {
+        id: 4,
+        path: '/images/slider/Slider-4.jpg'
+    },
+    {
+        id: 5,
+        path: '/images/slider/Slider-5.jpg'
+    },
+    {
+        id: 6,
+        path: '/images/slider/Slider-6.jpg'
+    },
+    {
+        id: 7,
+        path: '/images/slider/Slider-7.jpg'
+    },
+    {
+        id: 8,
+        path: '/images/slider/Slider-8.jpg'
+    },
+    {
+        id: 9,
+        path: '/images/slider/Slider-9.jpg'
+    },
+    {
+        id: 10,
+        path: '/images/slider/Slider-10.jpg'
+    },
+    {
+        id: 11,
+        path: '/images/slider/Slider-11.jpg'
+    },
+    {
+        id: 12,
+        path: '/images/slider/Slider-12.jpg'
+    },
+    {
+        id: 13,
+        path: '/images/slider/Slider-13.jpg'
+    },
+    {
+        id: 14,
+        path: '/images/slider/Slider-14.jpg'
+    },
+    {
+        id: 15,
+        path: '/images/slider/Slider-15.jpg'
+    },
+    {
+        id: 16,
+        path: '/images/slider/Slider-16.jpg'
+    },
+    {
+        id: 17,
+        path: '/images/slider/Slider-17.jpg'
+    },
+    {
+        id: 18,
+        path: '/images/slider/Slider-18.jpg'
+    },
+    {
+        id: 19,
+        path: '/images/slider/Slider-19.jpg'
+    },
+    {
+        id: 20,
+        path: '/images/slider/Slider-20.jpg'
+    },
+    {
+        id: 21,
+        path: '/images/slider/Slider-21.jpg'
+    },
+    {
+        id: 22,
+        path: '/images/slider/Slider-22.jpg'
+    },
+    {
+        id: 23,
+        path: '/images/slider/Slider-23.jpg'
+    },
+    {
+        id: 24,
+        path: '/images/slider/Slider-24.jpg'
+    },
+    {
+        id: 25,
+        path: '/images/slider/Slider-25.jpg'
+    },
+    {
+        id: 26,
+        path: '/images/slider/Slider-26.jpg'
+    },
+    {
+        id: 27,
+        path: '/images/slider/Slider-27.jpg'
+    },
+    {
+        id: 28,
+        path: '/images/slider/Slider-28.jpg'
+    },
+    {
+        id: 29,
+        path: '/images/slider/Slider-29.jpg'
+    },
+    {
+        id: 30,
+        path: '/images/slider/Slider-30.jpg'
+    },
+    {
+        id: 31,
+        path: '/images/slider/Slider-31.jpg'
+    },
+    {
+        id: 32,
+        path: '/images/slider/Slider-32.jpg'
+    },
+    {
+        id: 33,
+        path: '/images/slider/Slider-33.jpg'
+    },
+    {
+        id: 34,
+        path: '/images/slider/Slider-34.jpg'
+    },
+    {
+        id: 35,
+        path: '/images/slider/Slider-35.jpg'
+    },
+    {
+        id: 36,
+        path: '/images/slider/Slider-36.jpg'
+    },
+    {
+        id: 37,
+        path: '/images/slider/Slider-37.jpg'
+    },
+    {
+        id: 38,
+        path: '/images/slider/Slider-38.jpg'
+    },
+    {
+        id: 39,
+        path: '/images/slider/Slider-39.jpg'
+    }
+]
+
+export default Slides;
