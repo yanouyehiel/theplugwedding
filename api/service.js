04fa89c3367b4c7df1e315ccf9bfb82af7ec83d3
theplug23 <138583449+theplug23@users.noreply.github.com>
2023-07-12 16:31:01 +0100

    Add files via upload
    Mise a jour des styles et des donnees dans le dossier api

---
+++ api/service.js
@@ -0,0 +1,551 @@
+import simg1 from '/public/images/service/img-1.jpg'
+import simg2 from '/public/images/service/img-2.jpg'
+import simg3 from '/public/images/service/img-3.jpg'
+import simg4 from '/public/images/service/img-4.jpg'
+import simg5 from '/public/images/service/img-5.jpg'
+import simg6 from '/public/images/service/img-6.jpg'
+import simg7 from '/public/images/service/img-8.jpg'
+import simg8 from '/public/images/service/img-9.jpg'
+import simg9 from '/public/images/service/img-10.jpg'
+import simg10 from '/public/images/service/img-11.jpg'
+import simg11 from '/public/images/service/img-12.jpg'
+import simg12 from '/public/images/service/img-13.jpg'
+import simg13 from '/public/images/service/img-14.jpg'
+
+import sinnermg1 from '/public/images/service-single/2.jpg'
+import sinnermg2 from '/public/images/service-single/3.jpg'
+
+import singleImg1 from '/public/images/service-single/img-1.jpg'
+import singleImg2 from '/public/images/service-single/img-2.jpg'
+import singleImg3 from '/public/images/service-single/img-3.jpg'
+import singleImg4 from '/public/images/service-single/img-4.jpg'
+import singleImg5 from '/public/images/service-single/img-6.jpg'
+import singleImg6 from '/public/images/service-single/img-5.jpg'
+import singleImg7 from '/public/images/service-single/img-7.jpg'
+import singleImg8 from '/public/images/service-single/img-8.jpg'
+import singleImg9 from '/public/images/service-single/img-9.jpg'
+import singleImg10 from '/public/images/service-single/img-10.jpg'
+import singleImg11 from '/public/images/service-single/img-11.jpg'
+import singleImg12 from '/public/images/service-single/img-12.jpg'
+import singleImg13 from '/public/images/service-single/img-13.jpg'
+
+const Services = [
+    {
+        id: 1,
+        title:'Package 1: 399€ für 2 Stunden',
+        slug:'Package 1: 399€ für 2 Stunden',
+        packages: [
+            {
+                id: 1,
+                slug: '• Beratungsgespräch / Video Call'
+            },
+            {
+                id: 2,
+                slug: '• Fotovertrag'
+            },
+            {
+                id: 3,
+                slug: '• Anfahrt'
+            },
+            {
+                id: 4,
+                slug: '• Bis zu 2h Hochzeitsbegleitung'
+            },
+            {
+                id: 5,
+                slug: '• Professionelle Selektion & Bearbeitung aller Bilder'
+            },
+            {
+                id: 6,
+                slug: '• Mindestens 100 Bilder - unbegrenzt'
+            },
+            {
+                id: 7,
+                slug: '• Passwortgeschützte Online-Galerie für Gäste und Brautpaar'
+            },
+            {
+                id: 8,
+                slug: '• Eigener Ordner für Social Media Content'
+            },
+            {
+                id: 9,
+                slug: '• Luftaufnahme mit Drohne falls benötigt.'
+            }
+        ],
+        simg1:simg1,
+        ssImg:singleImg1,
+        sinnerImg1:sinnermg1,
+        sinnerImg2:sinnermg2,
+    },
+    {
+        id: 2,
+        title:'Package 2: 599€ für 3 Stunden',
+        slug:'Package 2: 599€ für 3 Stunden',
+        packages: [
+            {
+                id: 1,
+                slug: '• Beratungsgespräch / Video Call'
+            },
+            {
+                id: 2,
+                slug: '• Fotovertrag'
+            },
+            {
+                id: 3,
+                slug: '• Anfahrt'
+            },
+            {
+                id: 4,
+                slug: '• Bis zu 3h Hochzeitsbegleitung'
+            },
+            {
+                id: 5,
+                slug: '• Professionelle Selektion & Bearbeitung aller Bilder'
+            },
+            {
+                id: 6,
+                slug: '• Mindestens 200 Bilder - unbegrenzt'
+            },
+            {
+                id: 7,
+                slug: '• Passwortgeschützte Online-Galerie für Gäste und Brautpaar'
+            },
+            {
+                id: 8,
+                slug: '• Eigener Ordner für Social Media Content'
+            },
+            {
+                id: 9,
+                slug: '• Luftaufnahme mit Drohne falls benötigt.'
+            }
+        ],
+        simg1:simg2,
+        ssImg:singleImg2,
+        sinnerImg1:sinnermg1,
+        sinnerImg2:sinnermg2,
+    },
+    {
+        id: 3,
+        title:'Package 3: 799€ für 4 Stunden',
+        slug:'Package 3: 799€ für 4 Stunden',
+        packages: [
+            {
+                id: 1,
+                slug: '• Beratungsgespräch / Video Call'
+            },
+            {
+                id: 2,
+                slug: '• Fotovertrag'
+            },
+            {
+                id: 3,
+                slug: '• Anfahrt'
+            },
+            {
+                id: 4,
+                slug: '• Bis zu 4h Hochzeitsbegleitung'
+            },
+            {
+                id: 5,
+                slug: '• Professionelle Selektion & Bearbeitung aller Bilder'
+            },
+            {
+                id: 6,
+                slug: '• Mindestens 300 Bilder - unbegrenzt'
+            },
+            {
+                id: 7,
+                slug: '• Passwortgeschützte Online-Galerie für Gäste und Brautpaar'
+            },
+            {
+                id: 8,
+                slug: '• Eigener Ordner für Social Media Content'
+            },
+            {
+                id: 9,
+                slug: '• Luftaufnahme mit Drohne falls benötigt.'
+            }
+        ],
+        simg1:simg3,
+        ssImg:singleImg3,
+        sinnerImg1:sinnermg1,
+        sinnerImg2:sinnermg2,
+    },
+    {
+        id: 4,
+        title:'Package 4: 999€ für 5 Stunden',
+        slug:'Package 4: 999€ für 5 Stunden',
+        packages: [
+            {
+                id: 1,
+                slug: '• Beratungsgespräch / Video Call'
+            },
+            {
+                id: 2,
+                slug: '• Fotovertrag'
+            },
+            {
+                id: 3,
+                slug: '• Anfahrt'
+            },
+            {
+                id: 4,
+                slug: '• Bis zu 5h Hochzeitsbegleitung'
+            },
+            {
+                id: 5,
+                slug: '• Professionelle Selektion & Bearbeitung aller Bilder'
+            },
+            {
+                id: 6,
+                slug: '• Mindestens 400 Bilder - unbegrenzt'
+            },
+            {
+                id: 7,
+                slug: '• Passwortgeschützte Online-Galerie für Gäste und Brautpaar'
+            },
+            {
+                id: 8,
+                slug: '• Eigener Ordner für Social Media Content'
+            },
+            {
+                id: 9,
+                slug: '• Luftaufnahme mit Drohne falls benötigt.'
+            }
+        ],
+        simg1:simg4,
+        ssImg:singleImg4,
+        sinnerImg1:sinnermg1,
+        sinnerImg2:sinnermg2,
+    },
+    {
+        id: 5,
+        title:'Package 5: 1199€ für 6 Stunden',
+        slug:'Package 5: 1199€ für 6 Stunden',
+        packages: [
+            {
+                id: 1,
+                slug: '• Beratungsgespräch / Video Call'
+            },
+            {
+                id: 2,
+                slug: '• Fotovertrag'
+            },
+            {
+                id: 3,
+                slug: '• Anfahrt'
+            },
+            {
+                id: 4,
+                slug: '• Bis zu 6h Hochzeitsbegleitung'
+            },
+            {
+                id: 5,
+                slug: '• Professionelle Selektion & Bearbeitung aller Bilder'
+            },
+            {
+                id: 6,
+                slug: '• Mindestens 500 Bilder - unbegrenzt'
+            },
+            {
+                id: 7,
+                slug: '• Passwortgeschützte Online-Galerie für Gäste und Brautpaar'
+            },
+            {
+                id: 8,
+                slug: '• Eigener Ordner für Social Media Content'
+            },
+            {
+                id: 9,
+                slug: '• Luftaufnahme mit Drohne falls benötigt.'
+            }
+        ],
+        simg1:simg5,
+        ssImg:singleImg5,
+        sinnerImg1:sinnermg1,
+        sinnerImg2:sinnermg2,
+    },
+    {
+        id: 6,
+        title:'Package 6: 1399€ für 7 Stunden',
+        slug:'Package 6: 1399€ für 7 Stunden',
+        packages: [
+            {
+                id: 1,
+                slug: '• Beratungsgespräch / Video Call'
+            },
+            {
+                id: 2,
+                slug: '• Fotovertrag'
+            },
+            {
+                id: 3,
+                slug: '• Anfahrt'
+            },
+            {
+                id: 4,
+                slug: '• Bis zu 7h Hochzeitsbegleitung'
+            },
+            {
+                id: 5,
+                slug: '• Professionelle Selektion & Bearbeitung aller Bilder'
+            },
+            {
+                id: 6,
+                slug: '• Mindestens 600 Bilder - unbegrenzt'
+            },
+            {
+                id: 7,
+                slug: '• Passwortgeschützte Online-Galerie für Gäste und Brautpaar'
+            },
+            {
+                id: 8,
+                slug: '• Eigener Ordner für Social Media Content'
+            },
+            {
+                id: 9,
+                slug: '• Luftaufnahme mit Drohne falls benötigt.'
+            }
+        ],
+        simg1:simg6,
+        ssImg:singleImg6,
+        sinnerImg1:sinnermg1,
+        sinnerImg2:sinnermg2,
+    },
+    {
+        id: 7,
+        title:'Package 7: 1589€ für 8 Stunden',
+        slug:'Package 7: 1589€ für 8 Stunden',
+        packages: [
+            {
+                id: 1,
+                slug: '• Beratungsgespräch / Video Call'
+            },
+            {
+                id: 2,
+                slug: '• Fotovertrag'
+            },
+            {
+                id: 3,
+                slug: '• Anfahrt'
+            },
+            {
+                id: 4,
+                slug: '• Bis zu 8h Hochzeitsbegleitung'
+            },
+            {
+                id: 5,
+                slug: '• Professionelle Selektion & Bearbeitung aller Bilder'
+            },
+            {
+                id: 6,
+                slug: '• Mindestens 700 Bilder - unbegrenzt'
+            },
+            {
+                id: 7,
+                slug: '• Passwortgeschützte Online-Galerie für Gäste und Brautpaar'
+            },
+            {
+                id: 8,
+                slug: '• Eigener Ordner für Social Media Content'
+            },
+            {
+                id: 9,
+                slug: '• Luftaufnahme mit Drohne falls benötigt.'
+            }
+        ],
+        simg1:simg7,
+        ssImg:singleImg7,
+        sinnerImg1:sinnermg1,
+        sinnerImg2:sinnermg2,
+    },
+    {
+        id: 8,
+        title:'Package 8: 1789€ für 9 Stunden',
+        slug:'Package 8: 1789€ für 9 Stunden',
+        packages: [
+            {
+                id: 1,
+                slug: '• Beratungsgespräch / Video Call'
+            },
+            {
+                id: 2,
+                slug: '• Fotovertrag'
+            },
+            {
+                id: 3,
+                slug: '• Anfahrt'
+            },
+            {
+                id: 4,
+                slug: '• Bis zu 9h Hochzeitsbegleitung'
+            },
+            {
+                id: 5,
+                slug: '• Professionelle Selektion & Bearbeitung aller Bilder'
+            },
+            {
+                id: 6,
+                slug: '• Mindestens 800 Bilder - unbegrenzt'
+            },
+            {
+                id: 7,
+                slug: '• Passwortgeschützte Online-Galerie für Gäste und Brautpaar'
+            },
+            {
+                id: 8,
+                slug: '• Eigener Ordner für Social Media Content'
+            },
+            {
+                id: 9,
+                slug: '• Luftaufnahme mit Drohne falls benötigt.'
+            }
+        ],
+        simg1:simg8,
+        ssImg:singleImg8,
+        sinnerImg1:sinnermg1,
+        sinnerImg2:sinnermg2,
+    },
+    {
+        id: 9,
+        title:'Package 9: 1889€ für 10 Stunden',
+        slug:'Package 9: 1889€ für 10 Stunden',
+        packages: [
+            {
+                id: 1,
+                slug: '• Beratungsgespräch / Video Call'
+            },
+            {
+                id: 2,
+                slug: '• Fotovertrag'
+            },
+            {
+                id: 3,
+                slug: '• Anfahrt'
+            },
+            {
+                id: 4,
+                slug: '• Bis zu 10h Hochzeitsbegleitung'
+            },
+            {
+                id: 5,
+                slug: '• Professionelle Selektion & Bearbeitung aller Bilder'
+            },
+            {
+                id: 6,
+                slug: '• Mindestens 900 Bilder - unbegrenzt'
+            },
+            {
+                id: 7,
+                slug: '• Passwortgeschützte Online-Galerie für Gäste und Brautpaar'
+            },
+            {
+                id: 8,
+                slug: '• Eigener Ordner für Social Media Content'
+            },
+            {
+                id: 9,
+                slug: '• Luftaufnahme mit Drohne falls benötigt.'
+            }
+        ],
+        simg1:simg9,
+        ssImg:singleImg9,
+        sinnerImg1:sinnermg1,
+        sinnerImg2:sinnermg2,
+    },
+    {
+        id: 10,
+        title:'Package 10: 1989€ für 11 Stunden',
+        slug:'Package 10: 1989€ für 11 Stunden',
+        packages: [
+            {
+                id: 1,
+                slug: '• Beratungsgespräch / Video Call'
+            },
+            {
+                id: 2,
+                slug: '• Fotovertrag'
+            },
+            {
+                id: 3,
+                slug: '• Anfahrt'
+            },
+            {
+                id: 4,
+                slug: '• Bis zu 11h Hochzeitsbegleitung'
+            },
+            {
+                id: 5,
+                slug: '• Professionelle Selektion & Bearbeitung aller Bilder'
+            },
+            {
+                id: 6,
+                slug: '• Mindestens 1000 Bilder - unbegrenzt'
+            },
+            {
+                id: 7,
+                slug: '• Passwortgeschützte Online-Galerie für Gäste und Brautpaar'
+            },
+            {
+                id: 8,
+                slug: '• Eigener Ordner für Social Media Content'
+            },
+            {
+                id: 9,
+                slug: '• Luftaufnahme mit Drohne falls benötigt.'
+            }
+        ],
+        simg1:simg10,
+        ssImg:singleImg10,
+        sinnerImg1:sinnermg1,
+        sinnerImg2:sinnermg2,
+    },
+    {
+        id: 11,
+        title:'Package 11: 2189€ für 12 Stunden',
+        slug:'Package 11: 2189€ für 12 Stunden',
+        packages: [
+            {
+                id: 1,
+                slug: '• Beratungsgespräch / Video Call'
+            },
+            {
+                id: 2,
+                slug: '• Fotovertrag'
+            },
+            {
+                id: 3,
+                slug: '• Anfahrt'
+            },
+            {
+                id: 4,
+                slug: '• Bis zu 12h Hochzeitsbegleitung'
+            },
+            {
+                id: 5,
+                slug: '• Professionelle Selektion & Bearbeitung aller Bilder'
+            },
+            {
+                id: 6,
+                slug: '• Mindestens 1100 Bilder - unbegrenzt'
+            },
+            {
+                id: 7,
+                slug: '• Passwortgeschützte Online-Galerie für Gäste und Brautpaar'
+            },
+            {
+                id: 8,
+                slug: '• Eigener Ordner für Social Media Content'
+            },
+            {
+                id: 9,
+                slug: '• Luftaufnahme mit Drohne falls benötigt.'
+            }
+        ],
+        simg1:simg11,
+        ssImg:singleImg11,
+        sinnerImg1:sinnermg1,
+        sinnerImg2:sinnermg2,
+    }
+]
+export default Services;
---
+++ api/team.js
@@ -0,0 +1,38 @@
+import timg1 from '/public/images/team/img-1.jpg'
+import timg2 from '/public/images/team/img-2.jpg'
+import timg3 from '/public/images/team/img-3.jpg'
+import timg4 from '/public/images/team/img-4.jpg'
+
+const Teams = [
+    {
+       id:'1',
+       slug:'Margarate-Bennet',
+       tImg:timg1,
+       name:'Margarate Bennet',
+       title:'Dress Designer',     
+    },
+    {
+       id:'2',
+       slug:'Robert-William',
+       tImg:timg2,
+       name:'Robert William',
+       title:'Wedding Planner',      
+    },
+    {
+       id:'3',
+       slug:'Henry-D’suza',
+       tImg:timg3,
+       name:'Henry D’suza',
+       title:'Assistant',      
+    },
+    {
+       id:'4',
+       slug:'Jenefer-Abram',
+       tImg:timg4,
+       name:'Jenefer Abram',
+       title:'Lead Assistant',      
+    }
+    
+]
+
+export default Teams;
