c32272fc49c15d4bf91ff36fd32ecd96597476f7
theplug23 <138583449+theplug23@users.noreply.github.com>
2023-07-27 13:10:49 +0100

    Delete components directory
    Suppression du dossier components

--- components/UiStyle/FontAwesome.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const FontAwesome = ({name, classname=''}) => {
-    return (
-        <i className={`fa fa-${name} ${classname}`} />
-    )
-};
-
-export default FontAwesome;
